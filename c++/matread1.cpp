6017f14e5db31a809db9176ce711565f003905c1
Wirawan Purwanto <wirawan0@gmail.com>
2017-02-24 16:15:56 -0500

    * c++: added matrix read test.

---
+++ c++/matread1.cpp
@@ -0,0 +1,39 @@
+/************************************************************************\
+
+    matmul.cpp
+    Matrix multiplication demo
+
+    A demo HPC program
+
+\************************************************************************/
+
+#include <iostream>
+#include <fstream>
+#include <Matrix.hpp>
+
+void read1(const char *infile)
+//  read1: Reads a matrix from a file and prints it to stdout
+{
+    //using std::complex;
+
+    std::cout << "## read1 ## infile=" <<  infile <<std::endl;
+    std::ifstream inF(infile);
+
+    Matrix<double> A(readmatrix1<double>(inF));
+    //auto A = readmatrix1<double>(inF);
+    std::cout << "dimensions = " <<  A.numrows() << " " << A.numcols() <<std::endl;
+
+    printmatrix1(A, std::cout);
+}
+
+
+int main(int argc, const char **argv)
+{
+    if (argc < 2)
+    {
+        std::cerr << "Needs an input file name on arg1" << std::endl;
+        return 1;
+    }
+    read1(argv[1]);
+    return 0;
+}

df10d662b7649e76259fc0301f903add5cb6e389
Wirawan Purwanto <wpurwant@turing.hpc.odu.edu>
2021-08-04 11:32:50 -0400

    * Updated matread1.py to Python 3.

--- python/matread1.py
+++ python/matread1.py
@@ -3,13 +3,14 @@ matread1.py
 A demo program for reading in a matrix then printing it out
 """
 
+from __future__ import print_function
 import sys
 import numpy
 from np_helper import loadmatrix1, printmatrix1
 
 def read1(argv):
     if len(argv) < 2:
-        print >> sys.stderr, "Needs an input file name on arg1"
+        print("Needs an input file name on arg1", file=sys.stderr)
         sys.exit(1)
 
     matfile = argv[1]
